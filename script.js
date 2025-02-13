785ae7a3ec915d57f41a5cdbdea7f378b0564832
limlleonard <limlleonard@gmail.com>
2025-02-13 17:24:00 +0100

    iframe html modul

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // because of same source rule, it is not possible to add fetch translations from json files
+const switchButton = document.getElementById("language-switch");
+let currentLanguage = "de"; // Default language
+const savedLanguage = localStorage.getItem("language");
 const translations = {
     en: {
         welcome: "Welcome",
@@ -26,34 +29,35 @@ const translations = {
     }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    const switchButton = document.getElementById("language-switch");
-    let currentLanguage = "de"; // Default language
-
-    function updateLanguage(language) {
-        currentLanguage = language;
-        switchButton.textContent = language === "en" ? "DE" : "EN";
-        // Find all elements with translatable content
-        document.querySelectorAll("[data-i18n]").forEach((el) => {
-            const key = el.getAttribute("data-i18n");
-            el.textContent = translations[language][key] || key;
-        });
-        // Display page1 and hide page2 when de is selected
-        const page1=document.getElementById('page1');
-        const page2=document.getElementById('page2');
-        if (language==="en") {
-            page1.style.display='none';
-            page2.style.display='grid';
-        } else if (language==="de") {
-            page1.style.display='grid';
-            page2.style.display='none';
-        }
-        // Store the user's preference in localStorage
-        // localStorage.setItem("language", language);
+function updateLanguage(language) {
+    currentLanguage = language;
+    switchButton.textContent = language === "en" ? "DE" : "EN";
+    // Find all elements with translatable content
+    document.querySelectorAll("[data-i18n]").forEach((e) => {
+        const key = e.getAttribute("data-i18n");
+        e.textContent = translations[language][key] || key;
+    });
+    // Display page1 and hide page2 when de is selected
+    const page1=document.getElementById('page1');
+    const page2=document.getElementById('page2');
+    if (language==="en") {
+        page1.style.display='none';
+        page2.style.display='block';
+    } else if (language==="de") {
+        page1.style.display='block';
+        page2.style.display='none';
     }
+    // Store the user's preference in localStorage
+    // localStorage.setItem("language", language);
+}
 
-    // Load the saved language preference
-    const savedLanguage = localStorage.getItem("language");
+async function loadHTML(id, file) {
+    let response = await fetch(file);
+    let text = await response.text();
+    document.getElementById(id).innerHTML = text;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     if (savedLanguage) {
         updateLanguage(savedLanguage);
     }
@@ -63,14 +67,21 @@ document.addEventListener("DOMContentLoaded", () => {
         const newLanguage = currentLanguage === "en" ? "de" : "en";
         updateLanguage(newLanguage);
     });
+    loadHTML("projects", "projects.html");
 });
 
-function loadComponent(id, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        });
+function downloadPDF() {
+    const element = document.getElementById('page1');
+    // const element = document.body;
+    const opt = {
+        margin: 0,
+        filename: 'My_CV.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+    html2pdf().from(element).set(opt).save();
 }
+// document.getElementById('downloadPDF').addEventListener('click', downloadPDF);
 
-// loadComponent('navbar', './section/navbar.html');
+// python3 -m http.server 8000
